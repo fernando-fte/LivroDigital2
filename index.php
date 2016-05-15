b0aaeb0e516b275b8d41352b573d4e87262310b9
fernando-fte <fernando.fte@gmail.com>
2016-05-15 15:24:39 -0300

    Adiciona inclusão de paginas

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php include 'php/index.php'; ?>
 <!DOCTYPE html>
 <html>
+	<?php construct_page_required('xml', 'head'); ?>
 	<body>
 
+	<?php construct_page_required('xml'); ?>
 
 
+	<?php construct_page_required('xml', 'body_end'); ?>
 	</body>
 </html>
